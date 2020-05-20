6dec3a8847cbd60628fdbe83cc3985ab62759aee
Johannes Rehle <johannes.rehle@googlemail.com>
2020-05-20 16:23:55 +0200

    Revert Task Scheduler

--- Enable-PSLDAPSTask.ps1
+++ Enable-PSLDAPSTask.ps1
@@ -1,20 +1,10 @@
-param(
-    $ComputerName = "$env:COMPUTERNAME.$env:UserDNSDomain,test"
-)
+#TODO
+# - User 
 
-$actions = @()
-
-Foreach ($Computer in $ComputerName){
-    $argument = "-Command `"& 'C:\LDAP\Get-InsecureBinds.ps1' -p1 '$($Computer)' `""
-    $actions += New-ScheduledTaskAction -Execute 'PowerShell.exe' -Argument $argument
-}
-
+$argument = "-Command `"& 'C:\LDAP\Create-PSLDAPSLogging.ps1'`""
+$action = New-ScheduledTaskAction -Execute 'PowerShell.exe' -Argument $argument
 $principal = New-ScheduledTaskPrincipal -UserID "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount -RunLevel Highest
-
-# Task wird zur aktuellen Uhrzeit erstellt und jede Stunde wiederholt
 $trigger = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Hours 1)
-$task = New-ScheduledTask -Action $actions -Principal $principal -Trigger $trigger
+$task = New-ScheduledTask -Action $action -Principal $principal -Trigger $trigger
 Register-ScheduledTask -TaskName "N16 LDAP unsignierte Events" -InputObject $task -Force
-
-# Ausführen des Tasks
 Start-ScheduledTask -TaskName "N16 LDAP unsignierte Events"
