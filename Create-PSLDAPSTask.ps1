c726b310f4f0397c98cbb9ff859d5ae0429a7516
Johannes Rehle <johannes.rehle@googlemail.com>
2020-05-20 19:43:26 +0200

    Task Scheduler

--- Create-PSLDAPSTask.ps1
+++ Create-PSLDAPSTask.ps1
@@ -4,6 +4,10 @@ $trigger = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (Ne
 $Description='Dieser Task sammelt automatisiert die unverschlüsselten LDAP Aufrufe auf allen Domain Controller ein'
 $credential = Get-Credential
 $settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable -DontStopOnIdleEnd
+if (Get-ScheduledTask -TaskName $jobname -ErrorAction SilentlyContinue){
+    Write-Host "Task existiert bereits. Lösche den vorhanden Task"
+    Unregister-ScheduledTask -TaskName $jobname
+}
 Try{
     $ErrorActionPreference='stop'
     Register-ScheduledTask  -TaskName $jobname `
@@ -20,8 +24,10 @@ Catch{
     Write-Warning $_
 }
 
-If(Get-ScheduledTask -TaskName 'N16 LDAP unsignierte Events' -ErrorAction SilentlyContinue){
+If(Get-ScheduledTask -TaskName $jobname -ErrorAction SilentlyContinue){
     Write-Host 'Task has been created sucessfully' -BackgroundColor DarkBlue
 } else {
     Write-Host 'Task konnte nicht erstellt werden'
 }
+
+Start-ScheduledTask -TaskName $jobname
