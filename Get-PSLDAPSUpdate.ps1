e26644affe3419560b183661713e85066a8cc95d
Johannes Rehle <johannes.rehle@googlemail.com>
2020-07-02 14:42:10 +0200

    Update Version.txt

---
+++ Get-PSLDAPSUpdate.ps1
@@ -0,0 +1,33 @@
+$githubver = "https://raw.githubusercontent.com/johannes2704/PSLDAPSTools/master/version.txt"
+$version = "1.1.1"
+
+function UpdatesAvailable(){
+
+	$updateavailable = $false
+	$nextversion = $null
+	try	{
+		$nextversion = (New-Object System.Net.WebClient).DownloadString($githubver).Trim([Environment]::NewLine)
+	}	catch [System.Exception] {
+		Write-Host $_ "debug"
+	}
+	
+	Write-Host "CURRENT VERSION: $version"
+	Write-Host "NEXT VERSION: $nextversion"
+	if ($null -ne $nextversion -and $version -ne $nextversion){
+		#An update is most likely available, but make sure
+		$updateavailable = $false
+		$curr = $version.Split('.')
+		$next = $nextversion.Split('.')
+		for($i=0; $i -le ($curr.Count -1); $i++)		{
+			if ([int]$next[$i] -gt [int]$curr[$i]){
+				$updateavailable = $true
+				break
+			}
+		}
+	}
+	return $updateavailable
+}
+
+UpdatesAvailable
+
+
